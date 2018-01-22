0fad33bfe474bc44d2af76787110810ee08af58d
wuyudianye <1723030187@qq.com>
2018-01-22 19:22:58 +0800

    xinwen

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template> 
+  <div id="app">
+  	<div class="nav">
+  		<ul>
+  			<li class="active"><a href="javascript:;">首页</a>	
+  			</li>
+  			<li><a href="javascript:;">关注</a></li>
+  			<li><a href="javascript:;">栏目</a></li>
+  		</ul>
+  		
+  	</div>
+
+  </div>
+</template>
+
+<script>
+
+
+</script>
+
+<style>
+    @import url('./assets/css/index.css');
+</style>
